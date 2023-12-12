80e63a6c3a2fbc6e033624df4dcbdafde97468a2
JakubM <jakubmaladyn4@gmail.com>
2023-12-12 12:16:32 +0000

    fixing an commit issue

--- src/components/ListOfArticles.jsx
+++ src/components/ListOfArticles.jsx
@@ -2,23 +2,23 @@ import getArticles from "../utils/getArticles";
 import { useState ,useEffect} from "react";
 import Loading from "./Loading";
 import formatDate from "../utils/FormatDates";
+import { Link } from 'react-router-dom'
 
 const ListOfArticles = () => {
     const [articles,setArticles] = useState([]);
-
-    useEffect(() => {    
+    useEffect(() => {
         getArticles(setArticles)
     },[]);
-
     return <Loading>
         <ul>
             {articles.map(article => <li className='InListOfArticles' key={article.article_id}>
-                <div className="Title">{article.title} </div> 
+                <Link to={`/api/articles/${article.article_id}`}>
+                <div className="Title">{article.title} </div>
                 <div className="DateOfPost">{formatDate(article.created_at)}</div>
                 <div className="VotesOfPost">{article.votes} votes</div>
+                </Link>
                 </li> )}
         </ul>
     </Loading>
 };
-
 export default ListOfArticles;
