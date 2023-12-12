6635df867827609870848f965f9cf11ddef88317
JakubM <jakubmaladyn4@gmail.com>
2023-12-12 14:59:35 +0000

    fixed an error

---
+++ src/components/ArticlePage.jsx
@@ -0,0 +1,27 @@
+import getArticleByID from "../utils/getArticleByID";
+import { useState ,useEffect } from "react";
+import Loading from "./Loading";
+import { useParams } from 'react-router-dom'
+import formatDate from "../utils/FormatDates";
+import Comments from "./Comments";
+
+const ArticlePage = () => {
+    const [article,setArticle] = useState({});
+    const  {article_id} = useParams();
+
+    useEffect(() => {
+        getArticleByID(setArticle, article_id);
+    },[]);
+
+    return <Loading>
+        <div className="singleArticle">
+        <h2>{article.title}</h2>
+        <p>{article.body}</p>
+        <p>made by {article.author} at {formatDate(article.created_at)}</p>
+        <p>{article.votes} votes</p>
+        </div>
+        <Comments />
+    </Loading>
+};
+
+export default ArticlePage;
---
+++ src/components/VotesBox.jsx
@@ -0,0 +1,11 @@
+import getArticleByID from "../utils/getArticleByID";
+import { useState ,useEffect } from "react";
+import Loading from "./Loading";
+import { useParams } from 'react-router-dom'
+import formatDate from "../utils/FormatDates";
+import Comments from "./Comments";
+
+const Votes = () => {
+
+    return <button onClick={}></button>
+};
