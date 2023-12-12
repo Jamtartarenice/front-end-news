11dc7cf0e0cbec846a354bcea96584ac633ccd6e
JakubM <jakubmaladyn4@gmail.com>
2023-12-12 15:22:41 +0000

    fix merge issues

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,3 +1,4 @@
+import getArticleByID from "../utils/getArticleByID";
 import { useState ,useEffect } from "react";
 import { useParams } from 'react-router-dom'
 import formatDate from "../utils/FormatDates";
