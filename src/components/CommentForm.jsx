0dadf471264335d97a64648c01f861f92eff42a3
JakubM <jakubmaladyn4@gmail.com>
2023-12-14 15:49:22 +0000

    applied changes

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,50 +1,60 @@
-import { useState  } from "react";
+import React, { useState } from "react";
 import sendComment from "../utils/sendComment";
 
 const CommentForm = (props) => {
-    const article_id = props.article_id;
-    const comments = props.comments;
-    const [ commentInput, setCommentInput ] = useState('');
-    const [ username, setUsername ] = useState('');
-    
-    const handleUsernameChange = (event) => setUsername(event.target.value);
-    
-    const handleCommentChange = (event) => setCommentInput(event.target.value);
+  const article_id = props.article_id;
+  const comments = props.comments;
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        sendComment(article_id, commentInput, username)
-        .then(data => {
-            comments.push(data);
+  const [commentInput, setCommentInput] = useState("");
+  const [username, setUsername] = useState("grumpy19");
+  const [disableForm, setDisableForm] = useState(false);
+  const [offlineMessage, setOfflineMessage] = useState("");
+
+  const handleCommentChange = (event) => setCommentInput(event.target.value);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (navigator.onLine) {
+      setDisableForm(true);
+
+      sendComment(article_id, commentInput, username)
+        .then((data) => {
+          comments.push(data);
+          setCommentInput("");
+          setDisableForm(false);
+        })
+        .catch((error) => {
+          console.error("Error sending comment:", error);
+          setDisableForm(false);
         });
-        setCommentInput('');
-        setUsername('');
-    };
+    } else {
+      setOfflineMessage(
+        "You are currently offline. Your comment will be sent when the connection is restored."
+      );
+    }
+  };
 
-    return <><form className="commentForm" onSubmit={handleSubmit}>
-            <label htmlFor="username">Username:</label>
-            <input
-                type="text"
-                id="username"
-                name="username"
-                value={username}
-                onChange={handleUsernameChange}
-                placeholder="Enter your username"
-            />
-    
+  return <>
+        <form className="commentForm" onSubmit={handleSubmit}>
             <label htmlFor="comment">Comment:</label>
-            <input
+            <textarea
                 type="text"
                 id="comment"
                 name="comment"
                 value={commentInput}
                 onChange={handleCommentChange}
                 placeholder="Enter your comment"
+                disabled={disableForm}
             />
 
-            <button type="submit">Submit</button>
+            <button type="submit" disabled={commentInput.length === 0}>
+                Submit
+            </button>
         </form>
-        </>
-}
+
+        {offlineMessage && <p style={{ color: "red" }}>{offlineMessage}</p>}
+    </>
+};
 
 export default CommentForm;
